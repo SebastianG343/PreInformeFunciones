fe531374eaf05747c4162743bde493c9c07811a4
Duvan Díaz <47454957+KillTheReptiles@users.noreply.github.com>
2019-03-29 09:16:48 -0500

    Ejercicios

---
+++ 1.py
@@ -0,0 +1,13 @@
+def f():
+    a=int(input("Digite a"))
+    b=int(input("Digite b"))
+    a2=int(input("Digite a2"))
+    b2=int(input("Digite b2"))
+    x=(a**b)
+    xd=(a2**b2)
+    if x>xd:
+        a==x
+    else:
+        a==xd
+    print("R1=",x,"R2=",xd,"Resultado mayor:",a)
+f()
---
+++ 2.py
@@ -0,0 +1,16 @@
+def f():
+    lel=int(input("Digite un numero"))
+    if lel%1==0 and lel%lel==0:
+        if lel>4:
+            if lel%2==0 or lel%3==0:
+                print(lel,"Es primo")
+            else:
+                print(lel,"No es primo")
+        else:
+            if lel==2 or lel==3:
+                print(lel,"Es primo")
+            else:
+                print(lel,"No es primo")
+    else:
+        print(lel,"No es primo")
+f()
